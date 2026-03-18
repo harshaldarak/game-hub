cde8116d3d04d410b3814293f0327038de4a5fcd
Harshal Darak <harshalddarak09@gmail.com>
2026-03-19 00:16:17 +0530

    feat: Build search input and update logo

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,12 @@
 import { HStack, Image } from "@chakra-ui/react";
-import logo from "../assets/logo.png";
+import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
+import SearchInput from "./SearchInput";
 const NavBar = () => {
   return (
-    <HStack justifyContent='space-between' padding='10px'>
-      <Image src={logo} boxSize={"60px"} />
+    <HStack padding='10px'>
+      <Image src={logo} width={{ base: "42px", lg: "58px" }} />
+      <SearchInput />
       <ColorModeSwitch />
     </HStack>
   );
